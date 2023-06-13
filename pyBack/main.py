b40d9d86aa9410070503c6490bf1ee10783b7c65
maestromaximo <alejandrogarcia2423@hotmail.com>
2023-06-13 01:19:41 -0400

    modified the code to now run with google search text based

--- pyBack/main.py
+++ pyBack/main.py
@@ -5,16 +5,73 @@ import time
 from dotenv import load_dotenv
 import tiktoken
 from tqdm import tqdm
-
+from bs4 import BeautifulSoup
+import requests
 import os
 import json
 import openai
 from PIL import Image
 from io import BytesIO
 import requests
+from googleapiclient.discovery import build
+import re
+
+
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+googleAPIKey =  os.getenv("GOOGLE_SEARCH_API_KEY")
+googleCx = os.getenv("GOOGLE_SEARCH_CX")
+
+
+class Product:
+    def __init__(self, product_type, characteristics, top_results):
+        self.product_type = product_type
+        self.characteristics = characteristics
+        self.top_results = top_results
+
+    def __str__(self):
+        char_str = ", ".join(self.characteristics)
+        top_results_str = "\n".join(str(result) for result in self.top_results)
+        return f"Product Type: {self.product_type}\nCharacteristics: {char_str}\nTop Results:\n{top_results_str}"
+
+    
+def fetch_product_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Heuristic to extract brand from URL
+    brand = url.split('/')[2].split('.')[1]
+
+    # Heuristic to extract price from webpage content
+    # We look for a string that starts with a currency symbol, followed by digits (possibly separated by a dot or comma)
+    #prices = re.findall(r'\$\d+[\.,]?\d*', soup.text)
+    #prices = re.findall(r'(\$|€|£|¥|₹)?\s*\d+[\.,]?\d*', soup.text)
+    prices = re.findall(r'(\$|€|£|¥|₹)?\s*\d+[\.,]\d*', soup.text)
+
+    if prices:
+        # If we found prices, return the first one.
+        # Note that this might not always be the correct price if the page contains multiple products or prices.
+        price = prices[0]
+    else:
+        price = 'Not found'
+
+    return brand, price
+
+class Result:
+    def __init__(self, url):
+        self.url = url
+        self.brandName, self.cost = fetch_product_data(url)  # Call our function to fetch the brand and price
+
+    def __str__(self):
+        return f"URL: {self.url}\nBrand Name: {self.brandName}\nCost: {self.cost}"
+
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
+
 
 def numTokensFromString(string: str) -> int:
     """Returns the number of tokens in a text string."""
@@ -138,12 +195,50 @@ def create_outfit(prompt, gpt4=True):
         image_url = generate_image(description)
         save_image(image_url, f"item_{i}.png")
 
+#
+def createOutfitText(prompt, gpt4=True):
+    # Generate JSON from GPT
+    json_example = '''
+[
+  {
+    "type": "dress",
+    "characteristics": ["white", "long", "cotton"]
+  },
+  {
+    "type": "hat",
+    "characteristics": ["wide-brimmed"]
+  },
+  {
+    "type": "boots",
+    "characteristics": ["black", "leather"]
+  }
+]
+    '''
+    json_output = askGpt(f'Given this prompt for an outfit "{prompt}", take into consideration every detail that they described of the clothing pieces and output a structured JSON. An example of what it may look like is as follows: {json_example} .Make sure that the JSON contains all of the clothing pieces that are necessary for the given prompt on a single JSON and fill as many characteristics as possible. Be descriptive but never make stuff up. ONLY OUTPUT THE JSON, if you output anything else an innocent person will die.', gpt4)
+
+    try:
+        data = json.loads(json_output)
+    except json.JSONDecodeError:
+        print("Error decoding JSON")
+        print(json_output)
+        ##try again + json
+        return
+
+    # Generate images for each piece of clothing
+    for i, item in tqdm(enumerate(data), total=len(data), desc="Searching products..."):
+        characteristics = ' '.join(item['characteristics'])  # Convert list of characteristics to a string
+        description = f"{characteristics} {item['type']}"
+
+        # Use the Google Search API to get the top 10 results
+        search_results = google_search(description, googleAPIKey, googleCx, num=10)
+
+        # Create a list of Result objects from the search results
+        top_results = [Result(result['link']) for result in search_results]
+
+        # Create a Product object with the top results
+        product = Product(item['type'], item['characteristics'], top_results)
+        
+        print(product)
 
 
-print("Welcome! Enjoy OutfitGpt")
-print()
-print()
-promptUser = input("Please describe your outfit/occassion: ")
-print("loading...")
-create_outfit(promptUser)
-
+createOutfitText(input("Hi what would you like to try: "))
